8bb65160cac7fa16ec729970506675a12605cc50
Lucas Fontinele <contato@fontinele.dev>
2023-11-16 15:29:09 -0300

    chore: default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Global from "./styles/Global";
+import { ThemeProvider } from "styled-components";
+import Global from "./styles/global";
+import { defaultTheme } from "./styles/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <p>Hello World</p>
       <Global />
-    </>
+    </ThemeProvider>
   )
 }
 
